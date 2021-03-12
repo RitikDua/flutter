04bab056cf6bfd8aba432b023c3d170b5a20062d
RitikDua <rittikdua@gmail.com>
2021-03-12 18:38:55 +0530

    Infinite Scrolling

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  @override
+@override
   Widget build(BuildContext context) {
  	final wordPair=WordPair.random();
 	return MaterialApp(
-			title: 'Welcome to Flutter',
 			home:Scaffold(
 				appBar:AppBar(title: Text('Welcome to flutter'),	),
 			body:Center(child:RandomWords(),)
 			));
-  }
-}
+  } }
